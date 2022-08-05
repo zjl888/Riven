443a6653cb53c1503b4f548f1af406314fe798a6
zhujl <zjl2627758223@163.com>
2022-08-05 16:28:05 +0800

    首次提交

---
+++ r-Manager/src/main/java/com/riven/RManagerApplication.java
@@ -0,0 +1,13 @@
+package com.riven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RManagerApplication.class, args);
+    }
+
+}
---
+++ r-Manager/src/test/java/com/riven/RManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
